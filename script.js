382a109dcde9c82dcd7fee5df8fb8279028abcd7
Nathan <nwdamgaard@gmail.com>
2018-12-09 16:29:15 -0800

    added chooseScreen and implemented it

--- script.js
+++ script.js
@@ -2,3 +2,14 @@
 function opencardpack() {
     console.log("sup bois");
 }
+
+var currentScreen = "main-screen";
+function chooseScreen(newScreen) {
+    var cur_screen = document.getElementById(currentScreen);
+    var new_screen = document.getElementById(newScreen);
+
+    cur_screen.classList.remove("highlighted-screen");
+    new_screen.classList.add("highlighted-screen");
+
+    currentScreen = newScreen;
+}
