52468e5d3ac4c2497276b5e35bfb2e74a16afed8
Nathan <nwdamgaard@gmail.com>
2018-12-09 17:04:30 -0800

    load the new page in the iframe when user selects another page

--- script.js
+++ script.js
@@ -7,11 +7,14 @@ function opencardpack() {
 
 var currentScreen = "main-screen";
 function chooseScreen(newScreen) {
+    var iframe = document.getElementById("selectedScreen");
     var cur_screen = document.getElementById(currentScreen);
     var new_screen = document.getElementById(newScreen);
 
     cur_screen.classList.remove("highlighted-screen");
     new_screen.classList.add("highlighted-screen");
+
+    iframe.setAttribute("src", "screens/" + newScreen.slice(0, -7) + ".html");
 
     currentScreen = newScreen;
 }
